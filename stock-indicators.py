1e4b451aba0debd54c081411ae62ef02b1ac5a01
triarkya <lennard.heller@icloud.com>
2020-05-10 00:19:28 +0200

    added moving average calculation
    (works for multiple indicators)
    + added helper method to calculate averages if not enough data for interval

--- stock-indicators.py
+++ stock-indicators.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
 
+# calculates the average of values for a specififc subinterval from a list of numeric values
+# returns generator object (cast to list etc.)
+def values_to_avg(values, interval):
+    for i in range(1, len(values) + 1):
+        if len(values[:i]) < interval:
+            yield sum(values[:i]) / len(values[:i])
+        else:
+            yield sum(values[i - interval:i]) / len(values[i - interval:i])
+
+
 class StockDf:
     def __init__(self, open_price, high_price, low_price, close_price, volume):
         self.df = pd.DataFrame()
@@ -14,4 +24,9 @@ class StockDf:
     def set_middle(self):
         self.df["middle"] = (self.df["open"] + self.df["high"] + self.df["low"] + self.df["close"]) / 4
 
-    
+    # calculates moving average for an indicator (close by default)
+    # and time interval (14 steps by default)
+    def set_ma(self, indicator="close", interval=14):
+        self.df["ma_" + indicator + "_" + str(interval)] = list(
+            values_to_avg(self.df[indicator], interval)
+        )
